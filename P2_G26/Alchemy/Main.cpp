2d4f8375c1e4ce9dd9f731b427d7d14ff3ffd0bc
User <dsartur98@gmail.com>
2017-04-29 00:23:55 +0200

    el substr de elementos.first no chuta

--- P2_G26/Alchemy/Main.cpp
+++ P2_G26/Alchemy/Main.cpp
@@ -6,20 +6,6 @@
 #include <cstdlib>
 #include<unordered_map>
 
-void printHelp() {
-	std::cout << "-----------" << std::endl;
-	std::cout << "ALCHEMY BOY" << std::endl;
-	std::cout << "-----------" << std::endl;
-	std::cout << "- Enter two numbers of your elements list to combine them." << std::endl;
-	std::cout << "- Enter the word 'add' and the number of an element to add a new instance of that element." << std::endl;
-	std::cout << "- Enter 'add basics' to add new instances of the 4 basic elements" << std::endl;
-	std::cout << "- Enter the word 'delete' and the number of an element to erase it from your list" << std::endl;
-	std::cout << "- Enter the word 'info' and the number of an element to get information about it in the explorer." << std::endl;
-	std::cout << "- Enter the word 'sort' to sort by alphabetical order the elements in the list" << std::endl;
-	std::cout << "- Enter the word 'clean' to delete all the instances of repeated elements." << std::endl;
-	std::cout << "- Enter the word 'help' to show this tutorial." << std::endl;
-}
-
 template<>
 struct std::hash<std::pair<std::string, std::string>> {
 	size_t operator()(const std::pair<std::string, std::string>&p)const
@@ -31,57 +17,68 @@ struct std::hash<std::pair<std::string, std::string>> {
 
 std::unordered_map<std::pair<std::string, std::string>, std::string>elementosFinales;
 
+//GUARDA EN UN PAIR LOS ELEMENTOS QUE SE SUMADOS GENERARAN EL VALUE UN ELEMENTO EN FIRST Y OTRO EN SECOND
 std::pair<std::string, std::string>GuardarKey(std::string l) {
-	
-	std::pair<std::string, std::string>elementos;
-	int pos1 = l.find('+');
-	
-	elementos.first = l.substr(l.find('='), pos1);
-	elementos.second = l.substr(pos1, l.find('\n'));
 
+	std::pair<std::string, std::string>elementos;
+
+	elementos.first = l.substr(l.find("=") + 2, l.find("+"));
+	elementos.second = l.substr(l.find("+") + 2, l.find("\n") - 1);
+	
 	return elementos;
 }
 
+//GUARDA COMO VALUE EL ELEMENTO RESULTANTE DE LA SUMA DE DOS ELEMENTOS
 std::string GuardarValue(std::string l) {
-	
-	int primerElemento;
-	primerElemento = l.find_first_of(" ");
-	l = l.substr(0, primerElemento);
-
+	l = l.substr(0, l.find_first_of(" "));
 	return l;
 }
 
-
+//LEE EL ARCHIVO
 void leerArchivo(char lineas[]) {
 	
 	std::ifstream fentrada("elements.dat");
 
 	while (!fentrada.eof()) {
-		fentrada.getline(lineas, 250);
+		fentrada.getline(lineas, 1280);
 		elementosFinales[GuardarKey(lineas)] = GuardarValue(lineas);
 		} 
 	fentrada.close();
 }
 
+//PRINTEAR ELEMENTOS 
 void printElementos() {
 	for (auto i : elementosFinales)
 	{
-		std::cout << i.first.first << " " << i.first.second << " " << i.second << "\n";
+		std::cout << i.first.first << "/////" << i.first.second << "/////" << i.second <<std::endl;
 	}
 }
 
 
 
 void main() {
-	printHelp();
+	/*
+	//PRINT DE LA INFORMACION
+	std::cout << "-----------" << std::endl;
+	std::cout << "ALCHEMY BOY" << std::endl;
+	std::cout << "-----------" << std::endl;
+	std::cout << "Enter two numbers of your elements list to combine them." << std::endl;
+	std::cout << "Enter the word 'add' and the number of an element to add a new instance of that element." << std::endl;
+	std::cout << "Enter 'add basics' to add new instances of the 4 basic elements" << std::endl;
+	std::cout << "Enter the word 'delete' and the number of an element to erase it from your list" << std::endl;
+	std::cout << "Enter the word 'info' and the number of an element to get information about it in the explorer." << std::endl;
+	std::cout << "Enter the word 'sort' to sort by alphabetical order the elements in the list" << std::endl;
+	std::cout << "Enter the word 'clean' to delete all the instances of repeated elements." << std::endl;
+	std::cout << "-Enter the word 'help' to show this tutorial." << std::endl;*/
+	
 
-	char lineas[250];
+	char lineas[1280];
 	leerArchivo(lineas);
 
-	std::cin.clear(); 				  
-	std::cin.ignore(std::cin.rdbuf()->in_avail());
-	auto url= "https://en.wikipedia.org/wiki/fire";
-	ShellExecuteA(nullptr, "open", url, nullptr, nullptr, SW_SHOWNORMAL);
-
 	printElementos();
+
+	//std::cin.clear();
+	//std::cin.ignore(std::cin.rdbuf()->in_avail());
+	//auto url = "https://en.wikipedia.org/wiki/fire";
+	//ShellExecuteA(nullptr, "open", url, nullptr, nullptr, SW_SHOWNORMAL);
 }
