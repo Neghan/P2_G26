90320123ba47fede7b0626a10ad765d5ced86528
User <dsartur98@gmail.com>
2017-04-30 14:14:48 +0200

    funciones necesarias

--- P2_G26/Alchemy/Main.cpp
+++ P2_G26/Alchemy/Main.cpp
@@ -18,25 +18,29 @@ struct std::hash<std::pair<std::string, std::string>> {
 //MAP
 std::unordered_map<std::pair<std::string, std::string>, std::string>elementosFinales;
 
+//VECTOR
+std::vector<std::string>elementos({"Air","Earth","Fire","Water"});
+
+
 //GUARDA COMO VALUE EL ELEMENTO RESULTANTE DE LA SUMA DE DOS ELEMENTOS
 std::string GuardarValue(std::string l) {
 	l = l.substr(0, l.find_first_of(" "));
 	return l;
-}
+};
 
 //GUARDA EN UN PAIR LOS ELEMENTOS QUE SUMADOS GENERARAN EL VALUE, UN ELEMENTO EN FIRST Y OTRO EN SECOND
 std::pair<std::string, std::string>GuardarKey(std::string l) {
 
-	std::pair<std::string, std::string>elementos;
+	std::pair<std::string, std::string>keys;
 	auto pos = l.find("+");
 
-	elementos.first = l.substr(l.find("=") + 2, pos - 1);
-	elementos.first = elementos.first.substr(0, elementos.first.find("+") - 1);
+	keys.first = l.substr(l.find("=") + 2, pos - 1);
+	keys.first = keys.first.substr(0, keys.first.find("+") - 1);
 
-	elementos.second  = l.substr(pos + 2, l.find("\n") - 1);
-	
-	return elementos;
-}
+	keys.second  = l.substr(pos + 2, l.find("\n") - 1);
+
+	return keys;
+};
 
 //LEE EL ARCHIVO
 void leerArchivo(char lineas[]) {
@@ -48,7 +52,7 @@ void leerArchivo(char lineas[]) {
 		elementosFinales[GuardarKey(lineas)] = GuardarValue(lineas);
 	}
 	fentrada.close();
-}
+};
 
 //PRINTEAR ELEMENTOS 
 void printElementos() {
@@ -56,12 +60,9 @@ void printElementos() {
 	{
 		std::cout << "Elemento 1: " << i.first.first << " " << "Elemento 2: " << i.first.second << " " << "Elemento resultante: " << i.second <<std::endl;
 	}
-}
+};
 
-
-
-void main() {
-	/*
+void printHelp() {
 	//PRINT DE LA INFORMACION
 	std::cout << "-----------" << std::endl;
 	std::cout << "ALCHEMY BOY" << std::endl;
@@ -73,17 +74,78 @@ void main() {
 	std::cout << "Enter the word 'info' and the number of an element to get information about it in the explorer." << std::endl;
 	std::cout << "Enter the word 'sort' to sort by alphabetical order the elements in the list" << std::endl;
 	std::cout << "Enter the word 'clean' to delete all the instances of repeated elements." << std::endl;
-	std::cout << "-Enter the word 'help' to show this tutorial." << std::endl;*/
+	std::cout << "-Enter the word 'help' to show this tutorial." << std::endl;
+};
+
+void addElement() {};
+
+void addBasics() {};
+
+void deleteElement() {};
+
+void info() {
+	auto url = "https://en.wikipedia.org/wiki/" /*+ */;
+	ShellExecuteA(nullptr, "open", url, nullptr, nullptr, SW_SHOWNORMAL);
+};
+
+void sortElements() {
+	std::sort(elementos.begin(), elementos.end());
+};
+
+void cleanRepeatedElements() {
+
+};
+
+void leerInputJugador() {
+	std::string aux;
+	std::cin >> aux;
+
+	std::cin.clear();
+	std::cin.ignore(std::cin.rdbuf()->in_avail());
+
+	for (int i = 0; i < elementos.size(); ++i) {
+	if (aux == ("add " + char(i) )  ) {
+		addElement();
+		}
+	}
 	
+
+	if(aux == "add basics"){
+		addBasics();
+	}
+
+	if (aux == "delete") {
+		deleteElement();
+	}
+
+	if (aux == "info "/* + numero*/) {
+		info();
+	}
+
+	if (aux == "sort") {
+		sortElements();
+	}
+
+	if (aux == "clean") {
+		cleanRepeatedElements();
+	}
+
+	if (aux == "help") {
+		printHelp();
+	}
+};
+
+void combineElements() {
+	
+};
+
+void main() {
 
 	char lineas[300];
 	leerArchivo(lineas);
 
+	printHelp();
+	leerInputJugador();
 	printElementos();
 
-	/*std::cin.clear();
-	std::cin.ignore(std::cin.rdbuf()->in_avail());
-	auto url = "https://en.wikipedia.org/wiki/fire";
-	ShellExecuteA(nullptr, "open", url, nullptr, nullptr, SW_SHOWNORMAL);*/
-	
-}
+};
