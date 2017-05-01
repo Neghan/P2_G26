5cc56bcc3857b8c2e65e9b10f8064dd8c4a7f3dd
User <dsartur98@gmail.com>
2017-05-01 12:01:24 +0200

    c

--- P2_G26/Alchemy/Main.cpp
+++ P2_G26/Alchemy/Main.cpp
@@ -19,10 +19,10 @@ struct std::hash<std::pair<std::string, std::string>> {
 
 //MAP
 std::unordered_map<std::pair<std::string, std::string>, std::string>elementosFinales;
-
 //VECTOR
 std::vector<std::string>elementos({"Air","Earth","Fire","Water"});
-
+//SCORE
+int puntuacion = 0;
 
 //GUARDA COMO VALUE EL ELEMENTO RESULTANTE DE LA SUMA DE DOS ELEMENTOS
 std::string GuardarValue(std::string l) {
@@ -48,6 +48,13 @@ std::pair<std::string, std::string>GuardarKey(std::string l) {
 void leerArchivo(char lineas[]) {
 
 	std::ifstream fentrada("elements.dat");
+	/*	if () {
+		
+		std::cout << "Cannot read combination of elements from file elements.dat." << std::endl;
+		std::cout << "Check that it's placed in the same directory as Alchemy.exe!" << std::endl;
+		system("pause");
+	}*/
+
 
 	while (!fentrada.eof()) {
 		fentrada.getline(lineas, 300);
@@ -58,9 +65,11 @@ void leerArchivo(char lineas[]) {
 
 //PRINTEAR ELEMENTOS 
 void printElementos() {
-	for (auto &i : elementosFinales)
+	std::cout << "Your current score: " << puntuacion << std::endl;
+	std::cout << "You have these elements: " << std::endl;
+	for (int i = 0; i < elementos.size(); ++i)
 	{
-		std::cout << "Elemento 1: " << i.first.first << " " << "Elemento 2: " << i.first.second << " " << "Elemento resultante: " << i.second <<std::endl;
+		std::cout << i + 1 << ":" << elementos[i] << std::endl;
 	}
 };
 
@@ -69,14 +78,14 @@ void printHelp() {
 	std::cout << "-----------" << std::endl;
 	std::cout << "ALCHEMY BOY" << std::endl;
 	std::cout << "-----------" << std::endl;
-	std::cout << "Enter two numbers of your elements list to combine them." << std::endl;
-	std::cout << "Enter the word 'add' and the number of an element to add a new instance of that element." << std::endl;
-	std::cout << "Enter 'add basics' to add new instances of the 4 basic elements" << std::endl;
-	std::cout << "Enter the word 'delete' and the number of an element to erase it from your list" << std::endl;
-	std::cout << "Enter the word 'info' and the number of an element to get information about it in the explorer." << std::endl;
-	std::cout << "Enter the word 'sort' to sort by alphabetical order the elements in the list" << std::endl;
-	std::cout << "Enter the word 'clean' to delete all the instances of repeated elements." << std::endl;
-	std::cout << "-Enter the word 'help' to show this tutorial." << std::endl;
+	std::cout << "- Enter two numbers of your elements list to combine them." << std::endl;
+	std::cout << "- Enter the word 'add' and the number of an element to add a new instance of that element." << std::endl;
+	std::cout << "- Enter 'add basics' to add new instances of the 4 basic elements" << std::endl;
+	std::cout << "- Enter the word 'delete' and the number of an element to erase it from your list" << std::endl;
+	std::cout << "- Enter the word 'info' and the number of an element to get information about it in the explorer." << std::endl;
+	std::cout << "- Enter the word 'sort' to sort by alphabetical order the elements in the list" << std::endl;
+	std::cout << "- Enter the word 'clean' to delete all the instances of repeated elements." << std::endl;
+	std::cout << "- Enter the word 'help' to show this tutorial." << std::endl;
 };
 
 //1 2 = OTRO ELEMENTO
@@ -85,13 +94,23 @@ void combineElements(int myIndex1, int myIndex2) {
 		if (elementos[myIndex1] == it.first.first && 
 			elementos[myIndex2] == it.first.second) {
 			elementos.push_back(it.second);
+			puntuacion++;
+			std::cout << "New element found: " << elementos[elementos.size()] << std::endl;
+		}
+		else {
+			std::cout << "Combination failure, try again!" << std::endl;
 		}
 	}
 };
 
 //add 1 SE COPIA ESE ELEMENTO EN LA LISTA
 void addElement(int index) {
-	elementos.push_back(elementos[index]);
+	if (index <= elementos.size()) {
+		elementos.push_back(elementos[index]);
+	}
+	else {
+		std::cout << "You don't have this element!" << std::endl;
+	}
 };
 
 //AÑADE LOS 4 PRIMEROS ELEMENTOS
@@ -135,13 +154,11 @@ void leerInputJugador() {
 	std::string aux;
 	std::cin >> aux;
 
-	auto index1 = aux.substr(0, aux.find_first_of(" "));
-	auto index2 = aux.substr(aux.find_first_of(" "), aux.find("\n") - 1);
+	auto index1 = aux.substr(0, aux.find_first_of(" ") - 1);
+	auto index2 = index1.substr(0, index1.find("\n") - 1);
 
 	std::cin.clear();
 	std::cin.ignore(std::cin.rdbuf()->in_avail());
-
-	bool canCombine = false;
 		
 		if (aux == "add " + index2) {
 			addElement(std::stoi(index2));
@@ -183,4 +200,6 @@ void main() {
 	char lineas[300];
 	leerArchivo(lineas);
 	printHelp();
+	printElementos();
+	leerInputJugador();
 };
